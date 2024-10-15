672150f7a714f586742fbd414ad6f4f8beca9e10
Ben Sun <sunb26@mcmaster.ca>
2024-10-14 22:47:34 -0400

    ios initial setup

---
+++ iOS/HeartLink/HeartLink/HeartLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartLinkApp.swift
+//  HeartLink
+//
+//  Created by Ben Sun on 2024-10-14.
+//
+
+import SwiftUI
+
+@main
+struct HeartLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/HeartLink/HeartLinkTests/HeartLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeartLinkTests.swift
+//  HeartLinkTests
+//
+//  Created by Ben Sun on 2024-10-14.
+//
+
+import Testing
+@testable import HeartLink
+
+struct HeartLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
