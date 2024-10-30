e1cb8dc0ea8bb3a7b411041ce61db26307fe7eb3
Ben Sun <sunb26@mcmaster.ca>
2024-10-29 22:34:58 -0400

    add recording view placeholder

---
+++ iOS/HeartLink/RecordingView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordingView.swift
+//  HeartLink
+//
+//  Created by Ben Sun on 2024-10-29.
+//
+
+import SwiftUI
+
+struct RecordingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordingView()
+}
