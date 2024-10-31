6a354176bd4625688fc69729e1735e14811de9af
Ben Sun <sunb26@mcmaster.ca>
2024-10-31 17:18:08 -0400

    add recording view UI

--- iOS/HeartLink/HeartLink/RecordingView.swift
+++ iOS/HeartLink/HeartLink/RecordingView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct RecordingView: View {
+    @State var isRecording: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text(isRecording ? "Recording..." : "Ready to Record")
+                .font(.system(size: 34, weight: .bold))
+                .frame(maxWidth: .infinity, alignment: .center)
+                .frame(height: 400)
+            
+            
+            Button(action: {
+                isRecording.toggle()
+            }) {
+                if !isRecording {
+                    Image(systemName: "record.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.red)
+                } else {
+                    Image(systemName: "stop.circle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.red)
+                }
+            }
+        }
     }
 }
 
