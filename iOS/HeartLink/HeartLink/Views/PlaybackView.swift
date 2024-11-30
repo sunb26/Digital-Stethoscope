1e99fcc76363811a0849e35b703cc18437acc583
matt-wilker <mattw5871@rogers.com>
2024-11-30 01:30:48 -0500

    updatePlaybackView file

--- iOS/HeartLink/HeartLink/Views/PlaybackView.swift
+++ iOS/HeartLink/HeartLink/Views/PlaybackView.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 import AVFoundation
+import MobileCoreServices
+import UniformTypeIdentifiers
 
 struct PlaybackView: View {
     let url = URL(string: "http:192.168.2.206/heartlink.wav") // hard coding IP address temporarily
     let filename: String = "heartlink.wav" // hard coding .wav file name temporarily
     @State var downloadString: String = "Download File"
     @State var audioPlayer: AVAudioPlayer?
+    
+    
+    @State var show = false
     
     var body: some View {
         VStack{
@@ -41,6 +46,7 @@ struct PlaybackView: View {
             Button(action: {
                 // play file with specified file name from files -> downloads -> filename.wav
                 playWavFile()
+                self.show.toggle()
             })
             {
                 Image(systemName: "play.circle")
@@ -50,6 +56,9 @@ struct PlaybackView: View {
                     .foregroundColor(.black)
                     .frame(maxWidth: .infinity, alignment: .bottom)
             }
+            //.sheet(isPresented: $show) {
+              //  DocumentPicker()
+            //}
             Text(" ") // taking up white space
                 .font(.system(size: 42, weight: .bold))
                 .frame(maxWidth: .infinity, alignment: .center)
@@ -58,21 +67,67 @@ struct PlaybackView: View {
     }
     
     func playWavFile() {
-        guard let downloadsPath = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first
+        /*guard let downloadsPath = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first
         else {
             print("Error: Cannot access downloads folder")
             return
+        }*/
+        let downloadsPath = URL.downloadsDirectory
+        
+        do {
+            let items = try FileManager.default.contentsOfDirectory(at: downloadsPath, includingPropertiesForKeys: nil)
+            for item in items {
+                print("Item: \(item)")
+            }
+        } catch {
+            print("Error: \(error)")
         }
         
-        let wavFilePath = downloadsPath.appendingPathComponent(filename)
+        
+        let wavFilePath = downloadsPath.appending(components: filename)
+        let testBool = FileManager.default.fileExists(atPath: wavFilePath.path)
+        print("test bool: \(testBool)")
+        
+        //let wavFilePath = downloadsPath.appendingPathComponent(filename)
+        //let wavFilePath = URL(fileURLWithPath: "/Users/ec2-user/Library/Developer/CoreSimulator/Devices/AAA441DA-8B00-43EA-B3DB-5391E046036D/data/Containers/Data/Application/30F30C05-4033-4D81-827A-DEF7B69092FF/Downloads/heartlink.wav")
+        let accessing = wavFilePath.startAccessingSecurityScopedResource()
+        defer {
+            if accessing {
+                print("before")
+                wavFilePath.stopAccessingSecurityScopedResource()
+                print("after")
+            }
+        }
+        print("accessing: \(accessing)")
         //let wavFilePath = URL(string: ")
-        print("File Path: \(wavFilePath.absoluteString)")
+        print("File Path: \(wavFilePath.path)")
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: wavFilePath)
             audioPlayer?.play()
         } catch {
             print("Error when playing audio file: \(error)")
         }
+        do { wavFilePath.stopAccessingSecurityScopedResource()
+        }
+    }
+}
+
+//extension UTType {
+  //  static let wav = UTType(importedAs: "public.audio")
+//}
+
+struct DocumentPicker: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+        
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: [.wav])
+        picker.allowsMultipleSelection = false
+        //picker.directoryURL?.startAccessingSecurityScopedResource()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {
+        
     }
 }
 
