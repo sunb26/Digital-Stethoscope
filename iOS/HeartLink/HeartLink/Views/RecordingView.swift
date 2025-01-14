1983a378b95a1470b81564054e42beaa81bd305a
Ben Sun <sunb26@mcmaster.ca>
2025-01-14 15:25:41 -0500

    fix toggleRecording func call on every timer tick bug

--- iOS/HeartLink/HeartLink/Views/RecordingView.swift
+++ iOS/HeartLink/HeartLink/Views/RecordingView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct RecordingView: View {
+    @State var startRecording: Bool = false
     @State var isRecording: Bool = false
     @State var countdown: Int8 = 0
     @ObservedObject var bluetoothManager: BluetoothManager
     @State var timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
     
     func toggleRecording() {
-        let data = (isRecording ? "start" : "stop").data(using: .utf8)!
+        let data = (startRecording ? "start" : "stop").data(using: .utf8)!
         guard let char = bluetoothManager.recordingCharacteristic else {
             print("Could not find recording characteristic")
             return
@@ -26,22 +27,27 @@ struct RecordingView: View {
         if bluetoothManager.isConnected {
             ZStack{
                 VStack{
-                        
-                    Text(isRecording ? "Recording..." : "Ready to Record")    .font(.system(size: 42, weight: .bold))
+                    Text(startRecording ? "Recording..." : "Ready to Record")    .font(.system(size: 42, weight: .bold))
                         .frame(maxWidth: .infinity, alignment: .center)
                         .frame(height: 100)
                     
-                    Text(countdown == 0 ? " " : "Starts in: \(countdown)")
+                    Text(countdown <= 0 ? " " : "Starts in: \(countdown)")
                         .font(.system(size: 34, weight: .bold))
                         .frame(maxWidth: .infinity, alignment: .center)
                         .frame(height: 400, alignment: .top)
                     
                     Button(action: {
-                        isRecording.toggle()
-                        countdown = isRecording ? 3 : 0
+                        if startRecording {
+                            startRecording = false
+                            countdown = -1
+                            toggleRecording()
+                        } else {
+                            startRecording = true
+                            countdown = 3
+                        }
                     })
                     {
-                        if !isRecording { // ready to record page
+                        if !startRecording { // ready to record page
                             Image(systemName: "record.circle.fill")
                                 .resizable()
                                 .scaledToFit()
@@ -57,10 +63,13 @@ struct RecordingView: View {
                     }
                 }
                 .onReceive(timer) { time in // decrement timer every second
+                    guard startRecording else { return }
+                    
                     if countdown > 0 {
                         countdown -= 1
-                    } else {
-                        toggleRecording()
+                        if countdown == 0 {
+                            toggleRecording()
+                        }
                     }
                 }
             }
