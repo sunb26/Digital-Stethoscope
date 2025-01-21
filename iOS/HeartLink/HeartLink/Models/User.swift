873da534a2aad1327615d30a42b63eb23b5cedef
Ben Sun <sunb26@mcmaster.ca>
2025-01-20 22:46:40 -0500

    create User model

---
+++ iOS/HeartLink/HeartLink/Models/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  HeartLink
+//
+//  Created by Ben Sun on 2025-01-20.
+//
+
+import Foundation
+
+struct User: Decodable {
+    let email: String
+    let patientId: UInt64
+    let physicianId: UInt64
+    let widgets: [RecordingWidget]
+}
+
+enum LoginError: Error {
+    case invalidURL
+    case invalidCredentials
+    case invalidResponse
+    case invalidData
+    case serverError
+}
