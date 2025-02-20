fd81109b10852f26b9e93761ac233f2d2b85dd0a
Ben Sun <sunb26@mcmaster.ca>
2025-02-19 21:40:34 -0500

    web: add landing page logo

---
+++ web/components/hero.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+export const Hero = () => {
+  return (
+    <section>
+      <div className='flex flex-row justify-center justify-items-center'>
+        <Image
+          src="/logo.png"
+          width={720}
+          height={720}
+          alt="Picture of HeartLink Logo"
+          />
+      </div>
+    </section>
+  );
+}
+
