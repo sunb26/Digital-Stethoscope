b2f7ae3a14f844246ddd21436941378fc45f2f26
matt-wilker <mattw5871@rogers.com>
2025-02-21 23:04:40 -0500

    server: change endpoint1 output messages

--- server/endpoint1Pkg/endpoint1File.go
+++ server/endpoint1Pkg/endpoint1File.go
@@ -10,8 +10,36 @@ import (
 // SEE IF BETTER WAY THAN REDEFINING THIS AGAIN
 const keyServerAddr = "serverAddr" // const string used as key
 
+// GetEndpoint1 function
 func GetEndpoint1(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Start heartlinkServer endpoint1\n\n") // Write start message to response writer "w"
+
+	ctx := r.Context() // r.Context used to access the context of the request
+
+	// r.URL.Query used to access the query parameters in the URL
+	hasFirst := r.URL.Query().Has("first") // the Has method checks if a query parameter exists (returns Bool)
+	first := r.URL.Query().Get("first")    // the Get method retrieves the value of a query parameter (returns String)
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	// Printf writes the arguments extracted from URL to the console
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s(UPDATED)\n",
+		ctx.Value(keyServerAddr),
+		hasFirst, first,
+		hasSecond, second)
+
+	// Fprintf writes the arguments extracted from URL to the response writer "w"
+	fmt.Fprintf(w, "%s: got / request. first(%t)=%s, second(%t)=%s(UPDATED)\n",
+		ctx.Value(keyServerAddr),
+		hasFirst, first,
+		hasSecond, second)
+
+	fmt.Fprint(w, "End heartlinkServer endpoint1\n") // Write end message to response writer "w"
+}
+
+// PostEndpoint1 function
+func PostEndpoint1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Start heartlinkServer Postendpoint1\n\n") // Write start message to response writer "w"
 
 	ctx := r.Context() // r.Context used to access the context of the request
 
